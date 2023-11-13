b3f0dea160c51ff359847a2438fede2a528f7641
TomasMacoun <macount@seznam.cz>
2023-11-13 16:07:34 +0100

    initial commit

---
+++ Docker/rabbitMQLearning/src/test/java/com/example/rabbitmqlearning/RabbitMqLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rabbitmqlearning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitMqLearningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
