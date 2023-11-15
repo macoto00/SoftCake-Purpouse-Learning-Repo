d55e05a19585c7b7eb3e25c6901555bd68562f7a
TomasMacoun <macount@seznam.cz>
2023-11-15 12:14:02 +0100

    Update User.java

--- PostgreSQL/Spring Boot & PostrgeSQL Connection/postgreSQLConnection/src/main/java/com/example/postgresqlconnection/models/User.java
+++ PostgreSQL/Spring Boot & PostrgeSQL Connection/postgreSQLConnection/src/main/java/com/example/postgresqlconnection/models/User.java
@@ -1,4 +1,25 @@
 package com.example.postgresqlconnection.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "users")
 public class User {
+    @Id
+    @GeneratedValue
+    private Long id;
+    @NonNull
+    private String username;
+    @NonNull
+    private String password;
 }
